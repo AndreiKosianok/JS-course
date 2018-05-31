b1a453507f53c77d3b2b68487bab2d15143deff2
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-05-31 18:14:01 +0300

    поправил вывод на экран вместо консоли

--- Lesson_1_hard/js/script_hard.js
+++ Lesson_1_hard/js/script_hard.js
@@ -2,4 +2,4 @@ var num = 33721;
 	multipl = eval(num.toString().split('').join('*'));
 console.log(multipl);
 var powResult = (Math.pow(multipl, 3));
-console.log(powResult.toString().substring(0,2));
+alert(powResult.toString().substring(0,2));
