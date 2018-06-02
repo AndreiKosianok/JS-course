225904ec6844493685ac6f40182d16de6bc9393c
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-02 22:15:36 +0300

    ешё вариант

--- Lesson_3_hard/js/script_hard.js
+++ Lesson_3_hard/js/script_hard.js
@@ -1,30 +1,37 @@
 str = 'урок-3-был слишком легким';
 
-let upLetter = str.slice(0, 1).toUpperCase() + str.slice(1),
-	spaceReplace = upLetter.replace(/-/g, ' '),
+let upLetter = str.slice(0, 1).toUpperCase() + str.slice(1), //первый пункт задания
+	spaceReplace = upLetter.replace(/-/g, ' '), 			 //второй пункт задания
 	cutReplace = spaceReplace.replace(/легким/, ''),
-	lastReplace = cutReplace.substr(0, cutReplace.length - 3) + 'оо';
+	lastReplace = cutReplace.substr(0, cutReplace.length - 3) + 'оо'; //третий пункт задания
 console.log(lastReplace);
+
+//четвёртый пункт задания
 
 arr = [20, 33, 1, 'Человек', 2, 3];
 let total = 0;
 
-for (let i = 0; i<= arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
+	if (typeof(arr[i]) != 'string') {
 	total += Math.pow(arr[i], 3);
+	} else {
+		arr.splice([i],1);
+	}
 };
 console.log(Math.sqrt(total));
 
-let word = '   Введите какое-нибудь слово  ';
+//пятый пункт задания
 
 function seleteSpaces (text) {
 	if ((typeof(text)) === 'string' && text.length > 50) {
 		let textresult = text.trim().substr(0, 50) + '...';
 		console.log(textresult);
-	} else {
+	} else if ((typeof(text)) === 'string') {
+		let textresult = text.trim();
+		console.log(textresult);
+		} else {
 		alert('Повторите ввод');
-	};
+			}
 };
 
 seleteSpaces(prompt('Введите текст с пробелами в начале и конце'));
-
-
