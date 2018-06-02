9f453d2b1a3c63391a55c2853c6f9911f0364d74
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-02 22:39:04 +0300

    и ещё

--- Lesson_3_hard/js/script_hard.js
+++ Lesson_3_hard/js/script_hard.js
@@ -3,7 +3,7 @@ str = 'урок-3-был слишком легким';
 let upLetter = str.slice(0, 1).toUpperCase() + str.slice(1), //первый пункт задания
 	spaceReplace = upLetter.replace(/-/g, ' '), 			 //второй пункт задания
 	cutReplace = spaceReplace.replace(/легким/, ''),
-	lastReplace = cutReplace.substr(0, cutReplace.length - 3) + 'оо'; //третий пункт задания
+	lastReplace = cutReplace.slice(0, -3) + 'оо'; //третий пункт задания
 console.log(lastReplace);
 
 //четвёртый пункт задания
