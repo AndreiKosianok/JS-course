38932ecdbc936e8ffebc0d0f658222b39a2c3b6f
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-07 08:37:49 +0300

    задание 5 часть 2

--- Lesson_5/Task_2_project/js/script.js
+++ Lesson_5/Task_2_project/js/script.js
@@ -1,11 +1,10 @@
-let openButton = document.getElementById('open-btn');
+let openButton = document.getElementById('open-btn'); // Кнопка "открыть магазин"
 	mainLeftField = document.getElementsByClassName('main-info')[0]; //Это вывод их в консоль раскрывающимся списком, но я понятия не имею как с ним дальше работать
 	leftFieldsDiv = mainLeftField.getElementsByTagName('div'), //А этим костылём они 'по отдельности' в консоль вываливаются в виде массива
-	shopName = mainLeftField.getElementsByClassName('name');
 console.log(mainLeftField);
 console.log(leftFieldsDiv);
 
-let goodsItem = document.getElementsByClassName('goods-item');
+let goodsItem = document.getElementsByClassName('goods-item'); // Поля категорий товаров через цикл
 	for(let i = 0; i < goodsItem.length; i++) {
 		console.log(goodsItem[i]);
 		};
@@ -13,10 +12,10 @@ let goodsItem = document.getElementsByClassName('goods-item');
 let buttons = document.getElementsByTagName('button'); //На странице 4 кнопки
 console.log(buttons);
 
-let chooseItems = document.querySelector('.choose-item'),
+let chooseItems = document.querySelector('.choose-item'), // Поля ввода товаров, времени и расчета бюджета
 	currentTime = document.querySelector('.time-value'),
 	budgetCount = document.querySelector('.count-budget-value'),
-	hireEmployers = document.querySelectorAll('.hire-employers-item');
+	hireEmployers = document.querySelectorAll('.hire-employers-item'); // Поля имен сотрудников
 console.log(chooseItems);
 console.log(currentTime);
 console.log(budgetCount);
