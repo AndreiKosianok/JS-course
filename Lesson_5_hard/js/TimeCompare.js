55dd35c54aaf86d0ab8f0901118b2718e7f60f01
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-07 13:23:09 +0300

    последние штрихи

--- Lesson_5_hard/js/TimeCompare.js
+++ Lesson_5_hard/js/TimeCompare.js
@@ -2,17 +2,18 @@ function timeCompare() {
 	// получаем дату из форм
 let getStartDay = document.getElementById('dateStart').value.split('/'),
 	getEnd = document.getElementById('dateEnd').value.split('/'),
+	// переводим в нужный формат
 	dateStartCombined = new Date(getStartDay[2], getStartDay[0], getStartDay[1]),
 	dateEndCombined = new Date(getEnd[2], getEnd[0], getEnd[1]);
+	// условия расчёта
+	if (dateStartCombined.getTime() > dateEndCombined.getTime()) { // расчёты когда начальное число больше конечного
 
-	if (dateStartCombined.getTime() > dateEndCombined.getTime()) {
-		
 		let timeDifference = Math.abs(dateStartCombined.getTime() - dateEndCombined.getTime()),
 			DaysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
 
 		document.getElementById('dayCount').value = DaysDifference;
 
-	} else {
+	} else { //когда конечное больше начального
 
 		let timeDifference = Math.abs(dateEndCombined.getTime() - dateStartCombined.getTime()),
 			DaysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
