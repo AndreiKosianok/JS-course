4fd78edca65cd87956da1aff85e5d0d14ac8be80
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-12 21:14:37 +0300

    upd3

--- Lesson_7_hard/js/script.js
+++ Lesson_7_hard/js/script.js
@@ -1,20 +1,23 @@
 let	ball = document.getElementById('ball'),
 	start = document.getElementById('start'),
 	stop = document.getElementById('stop'),
+	animateBall;
 	position = 50;
-
 
 function ballMove() {
 	position = position + 50;
-	ball.style.left = (position + 'px');
+	ball.style.left = position + 'px';
 	if (position >= 900) {
 		position = 0;
 	}
-    requestAnimationFrame(ballMove)
+   animateBall = requestAnimationFrame(ballMove);
 }
+
+animateBall = requestAnimationFrame(ballMove);
+
 start.onclick = function() {
-	requestAnimationFrame(ballMove);
+    animateBall = requestAnimationFrame(ballMove);
 };
 stop.onclick = function() {
-	cancelAnimationFrame(ballMove);
+    cancelAnimationFrame(animateBall);
 };
