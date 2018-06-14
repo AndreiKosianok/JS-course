ccf81794377954c82daaec41a66626a465579042
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-14 09:07:59 +0300

    upd

--- Lesson_8/js/script.js
+++ Lesson_8/js/script.js
@@ -56,58 +56,60 @@ document.querySelector('.seconds').innerText = currentSeconds; // Текущее
 }, 1000);
 */
 
-let deadline = '2018-06-13';
+	let deadline = '2018-06-13';
 
-function getTimeRemaining(endtime){
+	function getTimeRemaining(endtime){
 
-	let t = Date.parse(endtime) - Date.parse(new Date()),
-		offset = -(new Date().getTimezoneOffset() / 60),
-			seconds = (addZero(Math.floor((t/1000) % 60))), 
-			minutes = (addZero(Math.floor((t/1000/60) % 60))),
-			hours = (addZero(Math.floor((t/(1000*60*60)) - offset)));
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			offset = new Date().getTimezoneOffset() / 60,
+				seconds = (addZero(Math.floor((t/1000) % 60))), 
+				minutes = (addZero(Math.floor((t/1000/60) % 60))),
+				hours = (addZero(Math.floor((t/(1000*60*60)) + offset)));
 
-			function addZero(dateNumber){
-				if (dateNumber >= 0 && dateNumber < 10) { 
-					return '0' + dateNumber;
-				} else {
-					return dateNumber;
+				function addZero(dateNumber){
+					if (dateNumber >= 0 && dateNumber < 10) { 
+						return '0' + dateNumber;
+					} else {
+						return dateNumber;
+					}
 				}
-			}
-			
-			return {
-				'total': t,
-				'hours': hours,
-				'minutes': minutes,
-				'seconds': seconds
-			};
-};
-
-function setClock(id, endtime){
-
-	let timer = document.getElementById(id),
-		hours = timer.querySelector('.hours'),
-		minutes = timer.querySelector('.minutes'),
-		seconds = timer.querySelector('.seconds');
 				
-		function updateClock(){
-			let t = getTimeRemaining(endtime);
-			hours.innerHTML = t.hours;
-			minutes.innerHTML = t.minutes;
-			seconds.innerHTML = t.seconds;
-
-			if(t.total <= 0){
-				clearInterval(timeInterval);
-				hours.innerHTML = '00',
-				minutes.innerHTML = '00',
-				seconds.innerHTML = '00';
-			}
-				
-		};
-
-	let timeInterval = setInterval(updateClock, 1000);
-	updateClock();
-				
+				return {
+					'total': t,
+					'hours': hours,
+					'minutes': minutes,
+					'seconds': seconds
+				};
 	};
+
+	function setClock(id, endtime){
+
+		let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds');
+					
+			function updateClock(){
+				let t = getTimeRemaining(endtime);
+				hours.innerHTML = t.hours;
+				minutes.innerHTML = t.minutes;
+				seconds.innerHTML = t.seconds;
+
+				if(t.total <= 0){
+					clearInterval(timeInterval);
+					hours.innerHTML = '00',
+					minutes.innerHTML = '00',
+					seconds.innerHTML = '00';
+				}
+					
+			};
+
+		let timeInterval = setInterval(updateClock, 1000);
+		updateClock();
+					
+	};
+
 	setClock('timer', deadline);
 
-});
+ });
+
