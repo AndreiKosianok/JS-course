3b61a6bb48860866620567d2a62d83cd7e42a614
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-16 00:30:53 +0300

    task 10

---
+++ Lesson_10/1/js/script.js
@@ -0,0 +1,183 @@
+window.addEventListener('DOMContentLoaded', function() {
+	let tab = document.getElementsByClassName('info-header-tab'),
+		tabContent = document.getElementsByClassName('info-tabcontent'),
+		info = document.getElementsByClassName('info-header')[0];
+
+	function hideTabContent(a) {
+		for (let i = a; i <tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+			console.log('hide done');
+		}
+	}
+
+	hideTabContent(1)
+
+	function showTabContent(b) {
+		if (tabContent[b].classList.contains('hide')) {
+			hideTabContent(0);
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+		}
+	}
+
+	info.addEventListener('click', (event) => {
+		let target = event.target;
+		if (target.className == 'info-header-tab') {
+			for (let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					showTabContent(i);
+					break;
+				}
+			}
+		};
+	})
+
+	let deadline = '2018-06-15';
+
+	function getTimeRemaining(endtime){
+
+		let offset = new Date().getTimezoneOffset() * 60000,
+			t = Date.parse(endtime) - Date.parse(new Date()) + offset,
+				seconds = (addZero(Math.floor((t/1000) % 60))), 
+				minutes = (addZero(Math.floor((t/1000/60) % 60))),
+				hours = (addZero(Math.floor((t/(1000*60*60)) + offset)));
+
+				function addZero(dateNumber){
+					if (dateNumber >= 0 && dateNumber < 10) { 
+						return '0' + dateNumber;
+					} else {
+						return dateNumber;
+					}
+				}
+				
+				return {
+					'total': t,
+					'hours': hours,
+					'minutes': minutes,
+					'seconds': seconds
+				};
+	};
+
+	function setClock(id, endtime){
+
+		let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds');
+					
+			function updateClock(){
+				let t = getTimeRemaining(endtime);
+				hours.innerHTML = t.hours;
+				minutes.innerHTML = t.minutes;
+				seconds.innerHTML = t.seconds;
+
+				if(t.total <= 0){
+					clearInterval(timeInterval);
+					hours.innerHTML = '00',
+					minutes.innerHTML = '00',
+					seconds.innerHTML = '00';
+				}
+					
+			};
+
+		let timeInterval = setInterval(updateClock, 1000);
+		updateClock();
+					
+	};
+
+	setClock('timer', deadline);
+
+	// Плавная прокрутка
+
+	let navigation = document.getElementsByTagName('nav')[0];
+
+	navigation.addEventListener('click', function(event) {
+		event.preventDefault();
+
+		animate(function (timePassed) {
+			let target = event.target,
+				section = document.getElementById( target.getAttribute('href').slice(1) );
+
+			window.scrollBy( 0, section.getBoundingClientRect().top / 20 - 3 );
+		}, 1500)
+	});
+
+	//модальное окно
+
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		close = document.querySelector('.popup-close'),
+		descrBtn = document.querySelectorAll('.description-btn'),
+		uA = window.navigator.userAgent,
+		isIE = /msie\s|trident\/|edge\//i.test(uA) && !!(document.uniqueID || document.documentMode || window.ActiveXObject || window.MSInputMethodContext || window.StyleMedia),
+		mobileBrowser = function detectmob() { 
+							if( navigator.userAgent.match(/Android/i)
+								|| navigator.userAgent.match(/webOS/i)
+								|| navigator.userAgent.match(/iPhone/i)
+								|| navigator.userAgent.match(/iPad/i)
+								|| navigator.userAgent.match(/iPod/i)
+								|| navigator.userAgent.match(/BlackBerry/i)
+								|| navigator.userAgent.match(/Windows Phone/i)
+							) {
+								return true;
+								} else {
+								return false;
+							  }
+						};
+
+
+	for(let i = 0; i < 4; i++){
+		descrBtn[i].addEventListener('click', function() {
+			this.classList.add('more-splash');
+			detect();
+		});
+	};
+
+	more.addEventListener('click', function() {
+		this.classList.add('more-splash');
+		detect();
+	});
+
+	close.addEventListener('click', function() {
+		overlay.style.display = "none";
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';
+	});
+
+	function detect() {
+		//this.classList.add('more-splash');
+		overlay.style.display = 'block';
+		document.body.style.overflow = 'hidden';
+		if (mobileBrowser() === false && !isIE) {
+				overlay.classList.remove('fade');
+				overlay.classList.add('some-animation');
+			} else if (mobileBrowser() === false) {
+				animate(function(timePassed) {
+					overlay.classList.remove('fade');
+					overlay.style.transform = 'rotate(' + timePassed + 'deg)'
+				}, 360);
+			} else {
+				overlay.classList.remove('fade');
+			}
+	}
+
+	function animate(draw, duration) {
+		let start = performance.now();
+
+		requestAnimationFrame(function animate(time) {
+			let timePassed = time - start;
+
+			if (timePassed > duration) {
+				timePassed = duration;
+			}
+			draw(timePassed);
+
+			if (timePassed < duration) {
+			requestAnimationFrame(animate);
+			}
+
+		});
+	}
+
+});
