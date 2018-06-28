0e5de509677eb6d143e360999906298f0c594711
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-06-28 17:06:32 +0300

    14 easy

--- Lesson_14/js/bundle.js
+++ Lesson_14/js/bundle.js
@@ -125,77 +125,33 @@ function modalWindow() {
 	let more = document.querySelector('.more'),
 		overlay = document.querySelector('.overlay'),
 		close = document.querySelector('.popup-close'),
-		descrBtn = document.querySelectorAll('.description-btn'),
-		uA = window.navigator.userAgent,
-		isIE = /msie\s|trident\/|edge\//i.test(uA) && !!(document.uniqueID || document.documentMode || window.ActiveXObject || window.MSInputMethodContext || window.StyleMedia),
-		mobileBrowser = function detectmob() { 
-							if( navigator.userAgent.match(/Android/i)
-								|| navigator.userAgent.match(/webOS/i)
-								|| navigator.userAgent.match(/iPhone/i)
-								|| navigator.userAgent.match(/iPad/i)
-								|| navigator.userAgent.match(/iPod/i)
-								|| navigator.userAgent.match(/BlackBerry/i)
-								|| navigator.userAgent.match(/Windows Phone/i)
-							) {
-								return true;
-								} else {
-								return false;
-							  }
-						};
+		descrBtn = document.querySelectorAll('.description-btn');
+		
 
-
-	for(let i = 0; i < 4; i++){
-		descrBtn[i].addEventListener('click', function() {
-			this.classList.add('more-splash');
-			detect();
-		});
+	function describeListen() {
+		for(let i = 0; i < 4; i++){
+			descrBtn[i].addEventListener('click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = 'block';
+				document.body.style.overflow = 'hidden';
+			});
+		};
 	};
 
-	more.addEventListener('click', function() {
-		this.classList.add('more-splash');
-		detect();
-	});
+	setInterval(describeListen, 50);
 
-	close.addEventListener('click', function() {
-		overlay.style.display = "none";
-		more.classList.remove('more-splash');
-		document.body.style.overflow = '';
-	});
-
-	function detect() {
-		//this.classList.add('more-splash');
-		overlay.style.display = 'block';
-		document.body.style.overflow = 'hidden';
-		if (mobileBrowser() === false && !isIE) {
-				overlay.classList.remove('fade');
-				overlay.classList.add('some-animation');
-			} else if (mobileBrowser() === false) {
-				animate(function(timePassed) {
-					overlay.classList.remove('fade');
-					overlay.style.transform = 'rotate(' + timePassed + 'deg)'
-				}, 360);
-			} else {
-				overlay.classList.remove('fade');
-			}
-	}
-
-	function animate(draw, duration) {
-		let start = performance.now();
-
-		requestAnimationFrame(function animate(time) {
-			let timePassed = time - start;
-
-			if (timePassed > duration) {
-				timePassed = duration;
-			}
-			draw(timePassed);
-
-			if (timePassed < duration) {
-			requestAnimationFrame(animate);
-			}
-
+		more.addEventListener('click', function() {
+			this.classList.add('more-splash');
+			overlay.style.display = 'block';
+			document.body.style.overflow = 'hidden';
 		});
-	}
+
+		close.addEventListener('click', function() {
+			overlay.style.display = "none";
+			more.classList.remove('more-splash');
+			document.body.style.overflow = '';
+		});
+
 }
 
 module.exports = modalWindow;
@@ -352,15 +308,15 @@ function tabs() {
 module.exports = tabs;
 },{}],8:[function(require,module,exports){
 function timer() {
-	let deadline = '2018-06-28';
+	let deadline = '2018-06-15';
 
 	function getTimeRemaining(endtime){
 
-		let offset = new Date().getTimezoneOffset() / 60000,
-			t = Date.parse(endtime) - Date.parse(new Date() + offset),
+		let offset = new Date().getTimezoneOffset() * 60000,
+			t = Date.parse(endtime) - Date.parse(new Date()) + offset,
 				seconds = (addZero(Math.floor((t/1000) % 60))), 
 				minutes = (addZero(Math.floor((t/1000/60) % 60))),
-				hours = (addZero(Math.floor(t/(1000*60*60))));
+				hours = (addZero(Math.floor((t/(1000*60*60)) + offset)));
 
 				function addZero(dateNumber){
 					if (dateNumber >= 0 && dateNumber < 10) { 
