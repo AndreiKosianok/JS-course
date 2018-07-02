a94022f89481587a5a704738982b552e45f68aed
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-07-03 00:37:52 +0300

    import list

--- Graduation_project/js/importList.js
+++ Graduation_project/js/importList.js
@@ -1,1 +1,11 @@
-import mainSlider from 'packParts/mainSlider.js';
+window.addEventListener('DOMContentLoaded', function() {
+	
+	let mainSlider = require('./packParts/mainSlider.js'),
+		modalWindows = require('./packParts/modalWindows.js'),
+		calculator = require('./packParts/calculator.js');
+	
+	mainSlider();
+	modalWindows();
+	calculator();
+	
+});
