aa4e106d4d211bf47fd9c0e7031b222972bf6139
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-07-03 00:28:02 +0300

    calculator

--- Graduation_project/js/script.js
+++ Graduation_project/js/script.js
@@ -159,6 +159,26 @@ window.addEventListener('DOMContentLoaded', function() {
 	})
 
 	//Калькулятор стоимости
+	let canvasSize = document.getElementById("size"),
+		canvasMaterial = document.getElementById("material"),
+		canvasOptions = document.getElementById("options"),
+		promocodeInput = document.getElementsByClassName("promocode")[0],
+		calcPriceField = document.querySelector(".calc-price"),
+		total = 0;
 
-	
+	canvasSize.addEventListener('change', priceCount);
+	canvasMaterial.addEventListener('change', priceCount);
+	canvasOptions.addEventListener('change', priceCount);
+	promocodeInput.addEventListener('change', priceCount);
+
+	function priceCount() {
+		total = (canvasSize.options.selectedIndex + canvasMaterial.options.selectedIndex) * 1000
+		if (canvasSize.options.selectedIndex !== 0 && canvasMaterial.options.selectedIndex !== 0) {
+			discountPopArt = promocodeInput.value == 'IWANTPOPART' ? 0.7 : 1
+			calcPriceField.innerHTML = (total + (canvasOptions.options.selectedIndex * 500)) * discountPopArt;
+		} else {
+			calcPriceField.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
+		}
+	}
+
 })
