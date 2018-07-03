fa9ea1c19cbd88266a12a1ad3597350bdddae467
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-07-03 21:46:06 +0300

    accordion

--- Graduation_project/js/script.js
+++ Graduation_project/js/script.js
@@ -294,6 +294,30 @@ window.addEventListener('DOMContentLoaded', function() {
 		feedbackSliderItem[feedbackSliderIndex - 1].className = 'feedback-slider-item animated fadeInLeft';
 	})
 
-	//
+	//Аккордеон //accordionHeading[i].style.display = "none"; "block"
+	let accordionHeading = document.querySelectorAll(".accordion-heading"),
+		accordionBlock = document.querySelectorAll(".accordion-block");
+
+	accordionHide();
+	function accordionHide() {
+		for (let j = 0; j< accordionBlock.length; j++) {
+				accordionBlock[j].style.display = "none";
+				accordionBlock[j].classList.add('animated', 'fadeInDown');
+			}
+	}
+
+	for (let i = 0; i< accordionHeading.length; i++) {
+		accordionHeading[i].addEventListener('click', function(event) {
+			if (accordionBlock[i].style.display === "none") {
+				accordionHide();
+				accordionBlock[i].style.display = "block";
+				accordionHeading[i].style.color = "rgb(197, 26, 187)";
+			} else {
+				accordionBlock[i].style.display = "none";
+				accordionHeading[i].style.color = "rgb(51, 51, 51)";
+			}
+		})
+	}
+
 
 })
