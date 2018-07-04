34d84171c4e61ac1427259733c03a2d67dae4384
AndreiKosianok <37742928+AndreiKosianok@users.noreply.github.com>
2018-07-04 19:29:48 +0300

    forms

--- Graduation_project/js/script.js
+++ Graduation_project/js/script.js
@@ -320,9 +320,17 @@ window.addEventListener('DOMContentLoaded', function() {
 		})
 	}
 
-	//Гамбургер-меню
+	//Гамбургер-меню 
+	//Реализация блока не завершена
 	let burgerMenu = document.querySelector(".burger-menu"),
 		burgerButton = document.querySelector(".burger");
+
+	displayCheck();
+	function displayCheck() {
+		if (document.documentElement.clientWidth > 768) {
+			burgerMenu.style.display = 'none';
+		}
+	}
 
 	burgerButton.addEventListener('click', function() {
 		burgerMenu.style.display = burgerMenu.style.display === 'block' ? 'none' : 'block';
@@ -331,5 +339,111 @@ window.addEventListener('DOMContentLoaded', function() {
 	burgerButton.addEventListener('touchstart', function() {
 		burgerMenu.style.display = burgerMenu.style.display === 'block' ? 'none' : 'block';
 	})
+
+	//AJAX
+		//phoneInputs = [form[2].querySelectorAll("input")[2], form[1].querySelectorAll("input")[0], form[0].querySelectorAll("input")[1]],
+		//nameInputs = [form[2].querySelectorAll("input")[1], form[1].querySelectorAll("input")[1], form[0].querySelectorAll("input")[0]],
+
+	let form = document.querySelectorAll("form"),
+		phoneInputs = document.querySelectorAll("[name='phone']"),
+		nameInputs = document.querySelectorAll("[name='name']"),
+		formTextareas = document.querySelectorAll("[name='message']"),
+		statusMessage = document.createElement('div');
+		statusMessage.classList.add('status');
+
+	let message = new Object();
+		message.loading = 'Загрузка...';
+		message.success = 'Спасибо! Скоро мы с вами свяжемся';
+		message.failure = 'Что-то пошло не так...';
+
+		//ожидание события для модального окна
+	for (i = 0; i < form.length; i++) {
+		form[i].addEventListener('submit', requestResponse);
+	};
+
+	for (i = 0; i < phoneInputs.length; i++) {
+		phoneInputs[i].addEventListener('input', mask);
+	};
+
+	for (i = 0; i < nameInputs.length; i++) {
+		nameInputs[i].onBlur = cyrilicOnly;
+		nameInputs[i].onkeyup = cyrilicOnly;
+	};
+
+	for (i = 0; i < formTextareas.length; i++) {
+		formTextareas[i].onBlur = cyrilicOnly;
+		formTextareas[i].onkeyup = cyrilicOnly;
+	};
+
+
+	function requestResponse(event) {
+		event.preventDefault();
+		this.appendChild(statusMessage);
+
+		//AJAX
+		let request = new XMLHttpRequest(),
+			formData = new FormData(event.target);
+
+		request.open('POST', '../mailer/smart.php');
+		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		request.send(formData);
+
+		request.onreadystatechange = function() {
+			if (request.readyState < 4) {
+				statusMessage.innerHTML = message.loading;
+			} else if (request.readyState === 4) {
+				if (request.status == 200 && request.status < 300) {
+					statusMessage.innerHTML = message.success;
+				} else {
+					statusMessage.innerHTML = message.failure;
+				}
+			}
+		}
+
+	};
+
+	//Маска ввода телефонного номера
+	let keyCode;
+		
+	function mask(event) {
+			event.KeyCode && (keyCode = event.keyCode);
+			let pos = this.selectionStart;
+			if (pos < 3) event.preventDefault();
+			let matrix = '(___) ____-___',
+				i = 0,
+				def = matrix.replace(/\D/g, ''),
+				val = this.value.replace(/\D/g, ''),
+				new_value = matrix.replace(/[_\d]/g, function(a) {
+					return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+				});
+
+		i = new_value.indexOf('_');
+		if (i != -1) {
+			i < 5 && (i = 1);
+			new_value = new_value.slice(0, i)
+		}
+
+		let reg = matrix.substr(0, this.value.length).replace(/_+/g, function(a) {
+				
+					return '\\d{1,' + a.length + '}'
+
+				}).replace(/[+()]/g, '\\$&');
+
+		reg = new RegExp('^' + reg +'$');
+
+		if (!reg.test(this.value) || this.value.length < 1 || keyCode > 47 && keyCode < 58) {
+			this.value = new_value;
+		}
+
+		if (event.type == 'blur' && this.value.length < 1) {
+			this.value = '';
+		}
+	}
+
+	//Фильтр только кирилица
+	function cyrilicOnly() {
+		this.value = this.value.replace(/[^\йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]/ig,"")
+	}
+
 
 })
